c3c83a557cc22d860a63431f368f734c460a96da
Shivamdumbare <86242001+Shivamdumbare@users.noreply.github.com>
2022-03-09 19:36:01 +0530

    Add files via upload

---
+++ Assisted practie/DiamondDemo/src/com/Simplilearn/Practice/DiamondProblem.java
@@ -0,0 +1,35 @@
+package com.Simplilearn.Practice;
+
+public class DiamondProblem {
+	interface First 
+	{  
+	    default void show() 
+	    { 
+	        System.out.println("Default First"); 
+	    } 
+	} 
+	interface Second 
+	{  
+	    default void show() 
+	    { 
+	        System.out.println("Default Second"); 
+	    } 
+	}  
+	public static class TestClass implements First, Second 
+	{  
+	    public void show() 
+	    {  
+	        First.super.show(); 
+	        Second.super.show(); 
+	    } 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+        TestClass ob = new TestClass(); 
+        ob.show(); 
+    } 
+}
+	}
+
+
